9d4603398bc3f0e2da8f01ee95ebb0f7290a5bcf
armandnjapou <armand2ngassu@gmail.com>
2022-01-25 19:48:22 +0100

    Add custom query to product repository

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -2,6 +2,8 @@ package com.ecommerce.microcommerce.dao;
 
 import com.ecommerce.microcommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface ProductDao extends JpaRepository<Product, Integer> {
     Product findById(int id);
+
+    @Query("SELECT id, name, price FROM Product p WHERE p.price = :priceLimit")
+    List<Product> findExpensiveProduct(@Param("priceLimit") int price);
 }
