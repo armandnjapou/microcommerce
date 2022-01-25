35061074b563e1781cb5a1e542adf6a4e453cb35
armandnjapou <armand2ngassu@gmail.com>
2022-01-25 19:22:42 +0100

    Add sql schema and initialise data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO product(id, name, price, buy_price) VALUES
+(1, 'PC', 750, 500),
+(2, 'Mac', 1500, 1300),
+(3, 'Smartphone', 900, 800);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE product
+(
+   id INT PRIMARY KEY,
+   name VARCHAR(255) NOT NULL,
+   price INT NOT NULL,
+   buy_price INT NOT NULL
+);
