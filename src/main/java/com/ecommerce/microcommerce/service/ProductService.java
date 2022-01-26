fc7c946609282d53bd0187a0c16b6a19bd78083a
armandnjapou <armand2ngassu@gmail.com>
2022-01-26 15:30:45 +0100

    Add service layer

---
+++ src/main/java/com/ecommerce/microcommerce/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.microcommerce.service;
+
+import com.ecommerce.microcommerce.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+    Product getProduct(int id);
+    Product saveOrUpdateProduct(Product product);
+    void deleteProduct(int id);
+}
---
+++ src/main/java/com/ecommerce/microcommerce/service/ProductServiceImpl.java
@@ -0,0 +1,35 @@
+package com.ecommerce.microcommerce.service;
+
+import com.ecommerce.microcommerce.dao.ProductDao;
+import com.ecommerce.microcommerce.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productDao.findAll();
+    }
+
+    @Override
+    public Product getProduct(int id) {
+        return productDao.findById(id);
+    }
+
+    @Override
+    public Product saveOrUpdateProduct(Product product) {
+        return productDao.save(product);
+    }
+
+    @Override
+    public void deleteProduct(int id) {
+        productDao.deleteById(id);
+    }
+}
